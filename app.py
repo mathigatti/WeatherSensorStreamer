67b961aa8caff46ddf86e3ee8cdff8e6196ed3cd
Mathias Gatti <mathias.gatti@mercadolibre.com>
2017-07-24 23:28:41 -0300

    graficos

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template
-import Adafruit_DHT  
+#import Adafruit_DHT  
+import pandas as pd
+import matplotlib.pyplot as plt
 
-app = Flask(__name__)
+import csv
 
-@app.route('/')
-def hello(name):
-	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4) 
+#app = Flask(__name__)
+
+def readFile(fileName):
+	df = pd.read_csv(fileName, parse_dates = True, index_col = 0, skiprows=[0])
+	df.head()
+	return df
+
+def saveFig(df, imgName):
+	plt.cla() # descarto datos
+	df.plot()
+	plt.savefig(imgName+".png")
+
+#@app.route('/')
+def index():
+	humidity = readFile('sensor-values/humidity_living-room_log_2017.csv')
+	temperature = readFile('sensor-values/humidity_living-room_log_2017.csv')
+	saveFig(humidity, "humedadIMG")
+	saveFig(temperature, "temperaturaIMG")
+	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
 	return render_template('page.html', humidity=humidity, temperature=temperature)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+	index()
+#    app.run(debug=True, host='0.0.0.0')
