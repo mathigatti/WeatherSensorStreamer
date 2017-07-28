7df17ea7d470530bed3f4e71606701c32210580b
Mathias Gatti <mathias.gatti@mercadolibre.com>
2017-07-28 11:14:15 -0300

    probando super sitio web

--- app.py
+++ app.py
@@ -12,19 +12,50 @@ def readFile(fileName):
 	df.head()
 	return df
 
-def saveFig(df, imgName):
-	plt.cla() # descarto datos
-	df.plot()
-	plt.savefig(imgName+".png")
-
 @app.route('/')
 def index():
 	humidity = readFile('sensor-values/humidity_living-room_log_2017.csv')
 	temperature = readFile('sensor-values/humidity_living-room_log_2017.csv')
-	saveFig(humidity, "humedadIMG")
-	saveFig(temperature, "temperaturaIMG")
+
 	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
-	return render_template('page.html', humidity=humidity, temperature=temperature)
+
+    graphs = [
+        dict(
+            data=[
+                dict(
+                    x=humidity.index,  # Can use the pandas data structures directly
+                    y=humidity
+                )
+            ],
+            layout=dict(
+                title='Historic Humidity'
+            )
+        ),
+        dict(
+            data=[
+                dict(
+                    x=temperature.index,  # Can use the pandas data structures directly
+                    y=temperature
+                )
+            ],
+            layout=dict(
+                title='Historic Temperature'
+            )
+        )
+    ]
+
+    # Add "ids" to each of the graphs to pass up to the client
+    # for templating
+    ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
+
+    # Convert the figures to JSON
+    # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
+    # objects to their JSON equivalents
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+	return render_template('layouts/index.html',
+                           ids=ids,
+                           graphJSON=graphJSON, humidity=humidity, temperature=temperature)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
